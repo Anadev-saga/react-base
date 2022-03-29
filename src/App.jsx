8dfc8c7f2f9848453eed62e35bd2ade879165f44
Ana Carolina Luz <anacarolinaluz@id.uff.br>
2022-03-28 21:36:39 -0300

    Setup

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import Footer from "./componentes/Footer";
+import Header from "./componentes/Header";
+
+const App = () => {
+  return (
+    <div>
+      <Header />
+      <p>Hello.</p>
+      <Footer />
+    </div>
+  );
+};
+
+export default App;
